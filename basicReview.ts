32f4cf0316780f2caeec34d86e176c1da29fed0b
Harbor Cat <837647312@qq.com>
2022-07-24 15:59:11 +0800

    基础语法复习

---
+++ basicReview.ts
@@ -0,0 +1,31 @@
+//对象类型, {}, Class, function, []
+const func = (str: string):number => {
+    return parseInt(str, 10);
+}
+
+const func1 = (str: string) => {
+    return parseInt(str, 10);
+}
+
+const func2:(str:string)=>number = (str)=>{
+    return parseInt(str, 10);
+}
+
+const data = new Date();
+
+//其他的case
+interface Person{
+    name:'String'
+}
+
+
+const rawData = '{"name":"dell"}';
+
+const newData:Person = JSON.parse(rawData);
+
+//Ts会认为temp永远是number
+let temp = 123;
+//temp = '456';
+
+let temp1: number | string = 123;
+temp1 = '456';
